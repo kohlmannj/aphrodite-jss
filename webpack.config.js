905b7e252b9c8571b1a23574b6cb06983b6c2b40
Joseph Kohlmann <kohlmannj@gmail.com>
2017-05-25 10:25:02 -0400

    Switch to scoped package + dependent on @kohlmannj/jss

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     alias: {
-      aphroditeJss: path.join(__dirname, 'src')
+      aphroditeJss: path.join(__dirname, 'src'),
+      jss: '@kohlmannj/jss'
     }
   }
 }
