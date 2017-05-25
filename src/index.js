bc2bb6ed481cddba72ba4c7332b8fffb683ed94f
Joseph Kohlmann <kohlmannj@gmail.com>
2017-05-25 18:06:09 -0400

    Add StyleSheet.render for immediate rendering

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const isNotFalsy = val => !!val
 const getClassName = rule => rule.className
 const generateClassName = (name, str) => `${name}-${hash(name + str + meta)}`
 const mergeStyles = (style, rule) => ({...style, ...rule.style})
-const defaultOptions = {insertionPoint: meta}
 
-export default function aphroditeJss(jss, options) {
-  const renderSheet = () => (
-    jss.createStyleSheet(null, {meta, ...options}).attach()
+export default function aphroditeJss(jss, options = {insertionPoint: meta}) {
+  const renderSheet = (styles = null) => (
+    jss.createStyleSheet(styles, {meta, ...options}).attach()
   )
 
+  let globalSheet
   let sheet = renderSheet()
 
   function css(...rules) {
@@ -33,24 +33,57 @@ export default function aphroditeJss(jss, options) {
     return className
   }
 
-  function register(styles) {
-    return Object.keys(styles).reduce((map, name) => {
-      map[name] = {
-        className: generateClassName(name, JSON.stringify(styles[name])),
-        style: styles[name]
+  function register(styles, addImmediately = false) {
+    const {classes, globals} = Object.keys(styles).reduce((map, name) => {
+      // If `name` is a word, i.e. not a special JSS property like `@import`, add it to map.classes
+      if (name.match(/^\w+$/) !== null) {
+        map.classes[name] = {
+          className: generateClassName(name, JSON.stringify(styles[name])),
+          style: styles[name]
+        }
       }
+      else {
+        map.globals[name] = styles[name]
+      }
+
       return map
-    }, {})
+    }, {classes: [], globals: {}})
+
+    if (Object.keys(globals).length > 0) {
+      // Immediately render the globals to globalSheet
+      if (typeof globalSheet !== 'object' || globalSheet === null) {
+        globalSheet = renderSheet(globals)
+      }
+      else {
+        globalSheet.addRules(globals)
+      }
+    }
+
+    if (addImmediately) {
+      return Object.keys(classes).reduce((renderedClasses, name) => {
+        const {className, style} = classes[name]
+        sheet.addRule(name, style, {className})
+        renderedClasses[name] = className
+        return renderedClasses
+      }, {})
+    }
+
+    return classes
   }
 
   function reset() {
+    // Detach and re-render the main sheet
     sheet.detach()
     jss.sheets.remove(sheet)
     sheet = renderSheet()
+    // Detach and re-render the global sheet
+    // Note: register() will init globalSheet if it needs to
+    globalSheet.detach()
+    jss.sheets.remove(globalSheet)
   }
 
   return {
-    StyleSheet: {create: register},
+    StyleSheet: {create: register, render: styles => register(styles, true)},
     toString: () => sheet.toString(),
     css,
     reset,
@@ -58,6 +91,4 @@ export default function aphroditeJss(jss, options) {
   }
 }
 
-export const {css, StyleSheet, reset, toString, version} = aphroditeJss(
-  create(preset()), defaultOptions
-)
+export const {css, StyleSheet, reset, toString, version} = aphroditeJss(create(preset()))
